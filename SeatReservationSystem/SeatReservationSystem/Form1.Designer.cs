2d87d33fc92df61ef3ee5a3bf75df2641acf518b
manuja1995 <manujan1995@gmail.com>
2017-06-14 12:09:25 +0530

    Add lable UserName

--- SeatReservationSystem/SeatReservationSystem/Form1.Designer.cs
+++ SeatReservationSystem/SeatReservationSystem/Form1.Designer.cs
@@ -28,21 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
             this.SuspendLayout();
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(23, 84);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(75, 19);
+            this.metroLabel1.TabIndex = 0;
+            this.metroLabel1.Text = "User Name";
             // 
             // Admin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(351, 299);
+            this.Controls.Add(this.metroLabel1);
             this.Name = "Admin";
             this.Text = "Admin";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private MetroFramework.Controls.MetroLabel metroLabel1;
     }
 }
 
--- SeatReservationSystem/SeatReservationSystem/Form1.cs
+++ SeatReservationSystem/SeatReservationSystem/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MetroFramework.Forms;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,7 +11,7 @@ using System.Windows.Forms;
 
 namespace SeatReservationSystem
 {
-    public partial class Admin : MetroFramework.Forms.MetroForm
+    public partial class Admin : MetroForm
     {
         public Admin()
         {
